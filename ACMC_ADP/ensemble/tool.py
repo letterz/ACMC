fe864c22b0e2ccbddcd5560d13a18a8f4fa7c2f4
Tian Zou <57440551+letterz@users.noreply.github.com>
2025-05-19 18:42:10 +0800

    Update tool.py

--- ACMC_ADP/ensemble/tool.py
+++ ACMC_ADP/ensemble/tool.py
@@ -6,18 +6,13 @@ def get_data_from_datasets(file_path):
     data_matrix = []
     with open(file_path, 'r') as file:
         for line in file:
-            # 去除行末的换行符然后以逗号分割数据
+           
             line_data = line.strip().split(',')
-            # 将除了最后一个数据之外的数据添加到列表
+           
             row_data = np.array([float(x) for x in line_data[:-1]])
             data_matrix.append(row_data)
             true_label.append(int(float(line_data[-1])))
-    # 转换成ndarray样式
+  
     data_matrix = np.array((data_matrix))
-    # feature_nums=data_matrix.shape[1]
-    # # 使用列表推导式生成列名
-    # column_names = [f'feature_{i}' for i in range(feature_nums)]
-    # #得到dataframe
-    # df = pd.DataFrame(data=data_matrix, columns=column_names)
 
     return data_matrix,true_label
