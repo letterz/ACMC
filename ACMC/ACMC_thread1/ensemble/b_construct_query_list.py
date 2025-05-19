e9491aee0d865ccf0e918129b195ce6afb74ee05
Tian Zou <57440551+letterz@users.noreply.github.com>
2025-05-19 16:54:16 +0800

    Update b_construct_query_list.py

--- ACMC/ACMC_thread1/ensemble/b_construct_query_list.py
+++ ACMC/ACMC_thread1/ensemble/b_construct_query_list.py
@@ -4,30 +4,18 @@ from operator import itemgetter
 from scipy.spatial.distance import euclidean
 
 def construct_query_list_for_max_uncertain_xi_list(max_uncertain_xi_list,  N,data):
-    """
-    :param max_uncertain_xi_list:
-    :param dist_matrix:
-    :param n:
-    :param N:
-    :return: 最大不确定度列表里每个点的查询列表字典
-    """
+
     query_list_dict={}
     for xi in max_uncertain_xi_list:
-        #为最大不确定度列表里的每个点构建查询队列
+       
         xi_query_list=construct_query_list_Q_for_xi(xi, N,data)
         query_list_dict[xi]=xi_query_list
-    # print(f'construct_query_list_for_max_uncertain_xi_list finished')
+    
     return query_list_dict
 
 def construct_query_list_Q_for_xi(max_uncertain_xi, neighborhood_N, data):
-    """
-    :param max_uncertain_xi: 在最大不确定度列表里的实例x*
-    :param neighborhood_N: 每个类别的邻域
-    :param data:
-数据集
-    :return: 查询列表 Q: [(xi, i_label), (xj, j_label)]
-    """
-    closest_x_list = []  # 用数组存储 (closest_instance, label, closest_dis)
+
+    closest_x_list = []  
     for label, neighbors in enumerate(neighborhood_N):
         closest_dis = float('inf')
         closest_instance = -1
@@ -42,8 +30,8 @@ def construct_query_list_Q_for_xi(max_uncertain_xi, neighborhood_N, data):
         if closest_instance != -1:
             closest_x_list.append([closest_instance, label, closest_dis])
 
-    # 按照距离进行排序
+ 
     closest_x_list.sort(key=lambda x: x[2])
-    # 返回去掉距离的结果
+
     closest_x = [(item[0], item[1]) for item in closest_x_list]
     return closest_x
