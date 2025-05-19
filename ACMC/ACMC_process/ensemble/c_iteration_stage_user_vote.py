2af970acf27c696818bf4004c69c5b182371feeb
Tian Zou <57440551+letterz@users.noreply.github.com>
2025-05-19 16:43:28 +0800

    Update c_iteration_stage_user_vote.py

--- ACMC/ACMC_process/ensemble/c_iteration_stage_user_vote.py
+++ ACMC/ACMC_process/ensemble/c_iteration_stage_user_vote.py
@@ -11,17 +11,15 @@ def process_xi(xi, query_list_dict, users_list, true_label, user_locks, min_user
     try:
         flag = 0
         xi_query_list = query_list_dict[xi]
-        # print(f"开始处理 xi={xi}")
 
         for one_tuple in xi_query_list:
             min_max = (min(xi, one_tuple[0]), max(xi, one_tuple[0]))
             seed = hash(min_max)
 
-            # 分配用户进行投票
             distributed_user_list = distribute_node_pair_to_users(users_list, min_users_num, max_users_num, seed=seed)
             voting_result = query_process(min_max[0], min_max[1], distributed_user_list, true_label, user_locks)
 
-            # 更新查询次数
+            
             with query_times_lock:
                 query_times_shared.value += len(distributed_user_list)
             with constraints_num_lock:
@@ -36,17 +34,17 @@ def process_xi(xi, query_list_dict, users_list, true_label, user_locks, min_user
                     print(
                         f"Index {one_tuple[1]} out of range for neighbourhoods_list_shared with length {len(neighbourhoods_list_shared)}")
                 else:
-                    # 必须链接，更新邻域信息
+                    
                     with neighbourhoods_lock:
-                        temp_list = neighbourhoods_list_shared[:]  # 读取共享列表
-                        temp_list[one_tuple[1]].append(xi)  # 修改本地列表
-                        neighbourhoods_list_shared[:] = temp_list  # 重新赋值回共享列表
-                        # print(f"xi={xi} 被加入 {neighbourhoods_list_shared[one_tuple[1]]}")
+                        temp_list = neighbourhoods_list_shared[:]  
+                        temp_list[one_tuple[1]].append(xi)  
+                        neighbourhoods_list_shared[:] = temp_list  
+                        
                     with label_dict_lock:
-                        temp_dict = dict(max_uncertain_xi_list_label_dict)  # 复制数据
-                        temp_dict[xi] = one_tuple[1]  # 修改
-                        max_uncertain_xi_list_label_dict.clear()  # 清空原始字典
-                        max_uncertain_xi_list_label_dict.update(temp_dict)  # 更新
+                        temp_dict = dict(max_uncertain_xi_list_label_dict)  
+                        temp_dict[xi] = one_tuple[1] 
+                        max_uncertain_xi_list_label_dict.clear() 
+                        max_uncertain_xi_list_label_dict.update(temp_dict) 
 
                     update_user_confidence_process(distributed_user_list, punish_user_index=vote_0_userlist,
                                                   user_locks=user_locks, isUpdate=True)
@@ -56,8 +54,8 @@ def process_xi(xi, query_list_dict, users_list, true_label, user_locks, min_user
         if flag == 0:
             with rest_point_lock:
                 rest_point.append(xi)
-                # print(f"xi={xi} 被加入 rest_point")
-        # print(f" xi={xi}处理完毕")
+               
+       
     except Exception as e:
         print(f"process_xi error for xi={xi}: {e}")
     return 0
@@ -80,27 +78,23 @@ def iteration_stage_user_vote_process(max_uncertain_xi_list, query_list_dict, us
                                                        constraints_num_lock, neighbourhoods_lock, label_dict_lock,
                                                        rest_point_lock))
                 for xi in max_uncertain_xi_list]
-
-            # 获取结果，确保任务完成
             for future in futures:
                 future.get()
 
         except Exception as e:
-            print(f"多进程池执行错误: {e}")
-            raise  # 重新抛出异常，便于调试
+            print(f"error: {e}")
+            raise 
 
-        # 处理剩余的点
+        # Process the remaining points.
         temp_nei, users_list, query_times, constraints_num = pre_cluster_user_vote_process(
             list(rest_point), users_list, true_label, min_users_num, max_users_num, query_times_shared, user_locks,
             constraints_num_shared,pool,query_times_lock,constraints_num_lock,neighbourhoods_lock, isUpdate)
-
-        # 添加新邻域到共享列表
         with neighbourhoods_lock:
             for one_nei in temp_nei:
                 neighbourhoods_list_shared.append(one_nei)
     except KeyboardInterrupt:
-        print("调试中断，正在释放资源...")
-        raise  # 重新抛出异常，让调试器捕获
+        print("Debugging interrupted; releasing resources...")
+        raise  
 
     return list(neighbourhoods_list_shared), users_list, query_times_shared.value, dict(
         max_uncertain_xi_list_label_dict), constraints_num_shared.value
